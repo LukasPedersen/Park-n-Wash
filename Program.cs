18bb6c9c6bc08a7d376f9889badf57ac6c1dc9f9
Lukas Pedersen <luka0591@elevcampus.dk>
2021-01-25 15:45:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Park_n_Wash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
