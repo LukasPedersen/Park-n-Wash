d6503ba3eb4587b398e9fe6683af001b7b1c47d1
Lukas Pedersen <luka0591@elevcampus.dk>
2021-02-05 11:12:28 +0100

    Added class to handle Car washing

---
+++ CarWash.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Park_n_Wash
+{
+    public class CarWash
+    {
+        List<Task> carsBeingWashed;
+        public CarWash()
+        {
+            new List<Task>();
+        }
+        public void WashNewCar()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -141,7 +141,6 @@ namespace Park_n_Wash
                     }
                 }
                 Console.Clear();
-
                 ConsoleHandler.WriteToConsole("Select ticket type\n", 20);
                 ConsoleHandler.WriteToConsole("\nDifference between ticket types\nGold ticket 500kr kr/t includes: Parking, car wash and better security\nSilver ticket 350kr kr/t includes: Parking and car wash\nBronze ticket 200kr kr/t includes: Parking\n", 20);
                 ConsoleHandler.WriteToConsole("1: Gold\n2: Silver\n3: Bronze\nX: Exit\n", 20);
